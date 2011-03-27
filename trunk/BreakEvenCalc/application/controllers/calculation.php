8831741527bb72f01ae1ffce2000e8124231daae
bharatkasodariya@gmail.com <bharatkasodariya@gmail.com@9c95222a-96c4-974c-001e-b68538cd9a5a>
2011-03-27 16:11:11 +0000

    merged version

---
+++ trunk/BreakEvenCalc/application/controllers/calculation.php
@@ -0,0 +1,155 @@
+<?php
+class Calculation extends CI_Controller{
+
+	function __construct() {
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->library('session');
+
+	}
+	
+	function index()
+	{
+				
+		$this->load->library('session');
+		$user=$this->session->userdata('username');
+		echo $user;
+		if($user=='')redirect('login');
+					
+		$totalSale= 0;
+			$costSale=0;
+			$fixCost=0;
+			$avarageSale=0;
+			$converisonRate=0;
+			$timePeriod = 0;
+			$data['companyName']=$this->session->userdata('companyName');						
+			$data['totalSale']=$totalSale;
+			$data['costSale']=$costSale;
+			$data['fixCost']=$fixCost;
+			$data['avarageSale']=$avarageSale;
+			$data['converisonRate']=$converisonRate;
+			$data['logoutUrl']='login/logout';
+			$data['userName']=$user;
+			$data['companyName']=$this->session->userdata('companyName');			
+			
+		$_REQUEST['FixedCost']=0;
+		$_REQUEST['breakeven']="10";
+
+		$_REQUEST['requestFrom']="test";
+		$_REQUEST['formsubmit']="true";
+		
+		$_REQUEST['TotalSale']=0;
+		$_REQUEST['CostofSale']=0;
+		$_REQUEST['AverageSale']=0;
+		$_REQUEST['ConversionRate']=0;
+		$_REQUEST['timePeriod']=0;
+		$data['cancelButton']=false;
+		if($user=='adminUser')
+			$data['cancelButton']=true;
+$data['expenseUrl']="expense/calcExpense";
+						
+			
+		$_REQUEST['urlValue']="calculation/formSubmit";
+		$_REQUEST['logoutUrl']="index.php/login/logout";
+		$_REQUEST['expenseUrl']="index.php/expense/calcExpense";
+		$this->load->view('CalculationForm',$data);
+		
+	}
+
+	function input($id = 0){
+		
+		$this->load->library('session');
+		$user=$this->session->userdata('username');
+		echo $user;
+		if($user!='adminUser')redirect('login/logout');
+		$this->load->helper('form');
+		$this->load->helper('html');
+		$this->load->model('calculation_model');
+	//$config['base_url'] = base_url().'index.php/calculation/input/';
+		//if($this->input->post('mysubmit')){
+		///	$this->books_model->entry_insert();
+		//}
+		//	$data = $this->books_model->general();
+		
+		if((int)$id > 0){
+						
+			$query = $this->calculation_model->get($id);
+			if($query['be_seq']=='')
+			{				
+				$_REQUEST['errorMessage']="Invalid Record";
+				redirect('error');
+			}
+			$data['fid']['value'] = $query['be_seq'];
+			
+			$totalSale= $query['be_total_sale'];
+			$costSale=$query['be_cost_sale'];
+			$fixCost=$query['be_fix_cost'];
+			$avarageSale=$query['be_avg_sale'];
+			$converisonRate=$query['be_conv_rate'];
+			$timePeriod = $query['be_time_period'];
+			$data['userName']=$query['be_create_by'];
+			$data['companyName']=$query['be_company'];
+			
+			$data['totalSale']=$totalSale;
+			$data['costSale']=$costSale;
+			$data['fixCost']=$fixCost;
+			$data['avarageSale']=$avarageSale;
+			$data['converisonRate']=$converisonRate;
+			$data['cancelButton']=true;
+			
+			$_REQUEST['FixedCost']=$fixCost;
+			$_REQUEST['breakeven']="10";
+
+		$_REQUEST['requestFrom']="test";
+		$_REQUEST['formsubmit']="true";
+		
+		$_REQUEST['TotalSale']=$totalSale;
+		$_REQUEST['CostofSale']=$costSale;
+		$_REQUEST['AverageSale']=$avarageSale;
+		$_REQUEST['ConversionRate']=$converisonRate;
+		$_REQUEST['timePeriod']=$timePeriod;
+		//$_REQUEST['logoutUrl']="/login/logout";
+		$data['logoutUrl']='../../login/logout';
+		$data['expenseUrl']='expense/calcExpense';
+
+		$_REQUEST['urlValue']="../../calculation/formSubmit";
+		$data['expenseUrl']="expense/calcExpense";
+		
+		$this->load->view('CalculationForm',$data);
+		
+		}
+		
+	}
+	
+	function formSubmit()
+	{
+		$this->load->library('session');
+		$user=$this->session->userdata('username');
+		echo $user;
+		if($user=='')redirect('login');
+		
+		$this->load->helper('form');
+		$this->load->helper('html');
+		
+//		echo $_REQUEST['FixedCost'];
+		
+//		$_REQUEST['requestFrom']='fromForm';
+		$data="";
+		$data['cancelButton']=false;
+		if($user=='adminUser')
+			$data['cancelButton']=true;
+		$data['logoutUrl']='../login/logout';
+		$data['companyName']=$this->session->userdata('companyName');			
+		
+		$data['userName']=$user;			
+		$_REQUEST['urlValue']=base_url()."index.php/calculation/formSubmit";
+		$_REQUEST['logoutUrl']=base_url()."index.php/login/logout";
+		$data['expenseUrl']="../expense/calcExpense";
+		
+		$this->load->view('CalculationForm',$data);
+	}
+	
+
+	
+}
+?>
---
+++ trunk/BreakEvenCalc/application/views/breakeven_view.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-us" lang="en-us">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href='<?php echo base_url(); ?>css/webcalc.css'
+	type="text/css" media="screen, projection" />
+	
+<title>Break Even List</title>
+</head>
+<body><form action="" method="post">
+
+<h1>Break Even Data</h1>
+		<table id="_id0:panelGrid" style="width: 70%; right: 80%" width="475px">
+
+
+
+<tbody>
+
+
+
+<tr>
+
+
+
+<td><span class="input"></span></td>
+
+
+
+<td></td>
+
+
+
+<td></td>
+
+
+
+<td></td>
+
+
+
+</tr>
+
+
+
+</tbody>
+
+
+
+</table>
+		<table id="_id0:body" 
+
+		style="height: auto; width: auto; border-bottom-style: solid; border-bottom-width: thick; border-top-style: solid; border-left-style: solid; border-right-width: thick; border-right-style: solid; border-left-width: thick; border-top-width: thick; border-left-color: LightSlateGray; border-top-color: LightSlateGray; border-right-color: LightSlateGray; border-bottom-color: LightSlateGray; background-attachment: fixed; background-color: #ebf3fd; background-position: center center; font-family: Verdana; font-size: 10px">
+
+
+
+<tbody>
+
+
+
+<tr>
+
+
+
+<td>
+
+
+
+
+
+<a href="login/logout"  id="_id0:_id6" style="color: #0000A0; font-size:12px">Logout</a></td>
+
+
+
+</tr>
+
+
+
+<tr>
+
+
+
+<td>
+
+
+
+<table id="_id0:data" border="10" cellpadding="5" cellspacing="3" frame="hsides" width="50%" style="height: auto; width: auto; border-bottom-style: none; border-bottom-width: thick; border-top-style: none; border-left-style: none; border-right-width: thick; border-right-style: none; border-left-width: thick; border-top-width: thick; border-left-color: #400040; border-top-color: #400040; border-right-color: #400040; border-bottom-color: #400040; background-attachment: fixed; background-color: #ebf3fd; background-position: center center" class="tableStyle">
+
+
+
+
+<?php echo $this->table->generate($results); ?>
+</table>
+</td>
+</tr>
+
+
+
+</tbody>
+
+
+
+</table>
+<table>
+<tr>
+<td>
+<?php echo $this->pagination->create_links(); ?>
+</td></tr>
+</table>
+
+
+</form>
+</body>
+</html>
