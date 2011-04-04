f5d9ce9dc919cb749e1bc9cbf5344615e9cb6893
bharatkasodariya@gmail.com <bharatkasodariya@gmail.com@9c95222a-96c4-974c-001e-b68538cd9a5a>
2011-04-04 19:06:12 +0000

    updated db

--- trunk/DB/db_setup.sql
+++ trunk/DB/db_setup.sql
@@ -1,12 +1,9 @@
- Server: localhost     Database: mysql     Table: break_even_data "InnoDB free: 4096 kB"
- 
-
 -- phpMyAdmin SQL Dump
 -- version 2.11.5
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 27, 2011 at 04:30 PM
+-- Generation Time: Apr 04, 2011 at 07:05 PM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.5
 
@@ -22,7 +19,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `break_even_login`
 --
 
-DROP TABLE IF EXISTS `break_even_login`;
 CREATE TABLE `break_even_login` (
   `be_username` varchar(50) NOT NULL,
   `be_password` varchar(50) NOT NULL,
@@ -34,6 +30,43 @@ CREATE TABLE `break_even_login` (
 --
 
 INSERT INTO `break_even_login` (`be_username`, `be_password`, `be_companyname`) VALUES
-('admin', 'password', 'test'),
-('test', 'test', 'test'),
-('adminUser', 'test', 'test');
+('administrator', 'password', 'admin'),
+('greg', 'test', 'testG'),
+('wendy', 'test', 'testW');
+
+
+
+--
+-- Database: `mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `break_even_data`
+--
+
+DROP TABLE IF EXISTS `break_even_data`;
+CREATE TABLE `break_even_data` (
+  `be_seq` int(10) NOT NULL auto_increment,
+  `be_total_sale` int(10) NOT NULL,
+  `be_cost_sale` int(10) default NULL,
+  `be_fix_cost` int(10) NOT NULL,
+  `be_avg_sale` int(10) NOT NULL,
+  `be_conv_rate` int(10) default NULL,
+  `be_date` date NOT NULL,
+  `be_create_by` char(50) NOT NULL,
+  `be_job_name` varchar(100) default NULL,
+  `be_time_period` int(10) default NULL,
+  `be_company` varchar(100) default NULL,
+  PRIMARY KEY  USING BTREE (`be_seq`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `break_even_data`
+--
+
+
+
+
+
